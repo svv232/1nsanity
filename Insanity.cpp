5457bd8a3fdf8aac37035d909212d7d39db89d04
Sai <svv232@nyu.edu>
2018-03-24 20:13:43 -0400

    completed bitwise and numerical obfuscation

--- Insanity.cpp
+++ Insanity.cpp
@@ -13,18 +13,22 @@ namespace {
         static char ID;
         Insanity(): FunctionPass(ID) {}
         virtual bool runOnFunction(Function &F) override;
-        Value * obfuscate(Instruction * I);
+        Value * obfuscateAdd(Instruction * I);
+        Value * obfuscateXor(Instruction * I);
+        Value * obfuscateOr(Instruction * I); 
+        Value * obfuscateAnd(Instruction * I); 
+        Value * obfuscateSub(Instruction * I);  
         void dumpAll(Function &F) const;
         void clean_up(std::vector<Instruction*>& clean);
+        Value * intToVal(const int intgr, 
+                Instruction * I);
         void replaceAll(Instruction * I, Value * V);
-        Value * valNeg(Value * oper);
-
     };
 
-Value * Insanity::valNeg(Value * oper){
-    auto cint = dyn_cast<ConstantInt>(oper);
-    return ConstantInt::get(oper -> getContext()
-            ,-(cint -> getValue()));
+Value * Insanity::intToVal(const int intgr, 
+        Instruction * I){
+    return ConstantInt::get(
+            Type::getInt32Ty(I -> getContext()),intgr);
 }
 
 void Insanity::replaceAll(Instruction * I, Value * V){
@@ -42,11 +46,53 @@ void Insanity::clean_up(std::vector<Instruction*>& clean){
     for (auto& I: clean){I -> eraseFromParent();}
 }
 
-Value * Insanity::obfuscate(Instruction * I){
+Value * Insanity::obfuscateAdd(Instruction * I){
     IRBuilder <> * builder = new IRBuilder<>(I);
-    //Value * ope2 = valNeg(I -> getOperand(1)); 
-    return builder -> CreateSub(I -> getOperand(0), 
-            I -> getOperand(1),"tmp"); 
+    Value * op1 = I -> getOperand(0);
+    Value * op2 = I -> getOperand(1);
+    op2 = builder -> CreateSub(intToVal(0,I), op2); 
+    return builder -> CreateSub(op1, op2); 
+}
+
+Value * Insanity::obfuscateXor(Instruction * I){
+    IRBuilder <> * builder = new IRBuilder<>(I);
+    Value * op1 = I -> getOperand(0);
+    Value * op2 = I -> getOperand(1); 
+    Value * negOp1=builder 
+        -> CreateXor(intToVal(-1, I), op1);
+    Value* andRes1=builder -> CreateAnd(negOp1, op2);
+    Value * negOp2=builder
+        -> CreateXor(intToVal(-1, I), op2);
+    Value * andRes2=builder -> CreateAnd(op1 ,negOp2);
+    return builder -> CreateOr(andRes1, andRes2);
+}
+
+Value * Insanity::obfuscateOr(Instruction * I){
+    IRBuilder <> * builder = new IRBuilder<>(I);
+    Value * op1 = I -> getOperand(0);
+    Value * op2 = I -> getOperand(1);
+    Value * andRes = builder -> CreateAnd(op1, op2);
+    Value * xorRes = builder -> CreateXor(op1, op2);
+    return builder -> CreateOr(andRes, xorRes);
+}
+
+
+Value * Insanity::obfuscateAnd(Instruction * I){
+    IRBuilder <> * builder = new IRBuilder<>(I);
+    Value * op1 = I -> getOperand(0);
+    Value * op2 = I -> getOperand(1);
+    Value * negOp1 = builder -> CreateXor(op2, 
+            intToVal(-1, I));
+    Value * xorRes = builder -> CreateXor(op1, negOp1);
+    return builder -> CreateAnd(xorRes, op1);
+}
+
+Value * Insanity::obfuscateSub(Instruction * I){
+    IRBuilder <> * builder = new IRBuilder<>(I);
+    Value * op1 = I -> getOperand(0);
+    Value * op2 = I -> getOperand(1);
+    op2 = builder -> CreateSub(intToVal(0,I), op2); 
+    return builder -> CreateAdd(op1, op2); 
 }
 
 bool Insanity::runOnFunction(Function &F){
@@ -57,11 +103,40 @@ bool Insanity::runOnFunction(Function &F){
         switch(I -> getOpcode()){
             case(Instruction::Add): {
                 auto& ins = *I;
-                replaceAll(&ins, obfuscate(&ins));
+                replaceAll(&ins, obfuscateAdd(&ins));
                 trash.push_back(&ins);
                 modified = true;
+                break;
             }
+            case(Instruction::Xor): {
+                auto& ins = *I;
+                replaceAll(&ins, obfuscateXor(&ins));
+                trash.push_back(&ins);
+                modified = true;
+                break;
             }
+            case(Instruction::Or): {
+                auto& ins = *I;
+                replaceAll(&ins, obfuscateOr(&ins));
+                trash.push_back(&ins);
+                modified = true;
+                break;
+            }
+            case(Instruction::And): {
+                auto& ins = *I;
+                replaceAll(&ins, obfuscateAnd(&ins));
+                trash.push_back(&ins);
+                modified = true;
+                break;
+            }
+            case(Instruction::Sub): {
+                auto& ins = *I;
+                replaceAll(&ins, obfuscateSub(&ins));
+                trash.push_back(&ins);
+                modified = true;
+                break;
+            }
+        }
         }
     clean_up(trash);
     dumpAll(F);
@@ -69,13 +144,11 @@ bool Insanity::runOnFunction(Function &F){
 }
 
 void Insanity::dumpAll(Function &F) const{
-    for (const auto &B : F){
-        for (const auto &I : B){
+    for (const auto &B : F)
+        for (const auto &I : B)
             I.dump();
-        }
-    }
 }
 }
 
 char Insanity::ID = 0;
-static RegisterPass<Insanity> X("1nsanity","1nsaneBof");
+static RegisterPass<Insanity> X("1nsanity","Insanity");
--- test.c
+++ test.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    int a, b, c, d;
+    int a, b, c, d, e;
     a = 17;
     b = 3;
     c = a + b;
     d = c * 2;
+    printf("%d \n",d);
+    e = d ^ c;
+    printf("%d \n",e);
+    a = 56;
+    b = a | e;
+    printf("%d \n",b);
+    c = b & a;
+    printf("%d \n",c);
+    d = c - 25;
     printf("%d \n",d);
     return 0;
 }
