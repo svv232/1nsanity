47072e33bffa6dfb7fcb5f3ee2952b9ece316776
Sai <svv232@nyu.edu>
2018-04-14 16:34:57 -0400

    Hailstone Opaque Predicate successfully obfuscates control flow

--- Insanity.cpp
+++ Insanity.cpp
@@ -3,34 +3,41 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/InstIterator.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Module.h"
+
+
 #include <vector>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace llvm;
 
 namespace {
-    struct Insanity: public FunctionPass {
-        
-        static char ID;
-        Insanity(): FunctionPass(ID) {}
-        virtual bool runOnFunction(Function &F) override;
-        Value * obfuscateAdd(Instruction * I);
-        Value * obfuscateXor(Instruction * I);
-        Value * obfuscateOr(Instruction * I); 
-        Value * obfuscateAnd(Instruction * I); 
-        Value * obfuscateSub(Instruction * I);  
-        void dumpAll(Function &F) const;
-        void clean_up(std::vector<Instruction*>& clean);
-        Value * intToVal(const int intgr, 
-                Instruction * I);
-        void replaceAll(Instruction * I, Value * V);
-    };
+struct Insanity: public FunctionPass {
+    static char ID;
+    Insanity(): FunctionPass(ID) {}
+    virtual bool runOnFunction(Function &F) override;
+    Value * obfuscateAdd(Instruction * I);
+    Value * obfuscateXor(Instruction * I);
+    Value * obfuscateOr(Instruction * I); 
+    Value * obfuscateAnd(Instruction * I); 
+    Value * obfuscateSub(Instruction * I);  
+    Value * obfuscateBr(BranchInst * I, Value * V);
+    void dumpAll(Function &F) const;
+    void clean_up(std::vector<Instruction*>& clean);
+    Value * intToVal(const int intgr, Instruction * I);
+    Value * intToVal(const int intgr, LLVMContext& C);
+    void replaceAll(Instruction * I, Value * V);
+    Value * insertHailStoneQuery(int n, Instruction * I);
+    Function * generateHailStone(Instruction * I);
+};
 
-Value * Insanity::intToVal(const int intgr, 
-        Instruction * I){
-    return ConstantInt::get(
-            Type::getInt32Ty(I -> getContext()),intgr);
+Value * Insanity::intToVal(const int intgr, Instruction * I){
+    return ConstantInt::get(Type::getInt32Ty(I -> getContext()),intgr);
 }
-
+Value * Insanity::intToVal(const int intgr, LLVMContext& C){
+    return ConstantInt::get(Type::getInt32Ty(C),intgr);
+}
 void Insanity::replaceAll(Instruction * I, Value * V){
     BasicBlock::iterator iter(I);
     Value * repl = dyn_cast<Value>(I);
@@ -40,6 +47,71 @@ void Insanity::replaceAll(Instruction * I, Value * V){
                 iter -> setOperand(i, V);
         }
     }
+}
+
+Function * Insanity::generateHailStone(Instruction * I){
+    Module * mod = I -> getModule();
+	Constant * c = mod -> getOrInsertFunction("hailstone", 
+                                            Type::getInt32Ty(mod -> getContext()),
+                                            Type::getInt32Ty(mod -> getContext()),
+                                            nullptr);
+    Function * hailStone = cast<Function>(c);
+	hailStone -> setCallingConv(CallingConv::C);
+    Function::arg_iterator args = hailStone->arg_begin();
+	Value * n_arg = &*args++;
+	n_arg -> setName("hailStoneN");
+    
+    BasicBlock * entry = BasicBlock::Create(I -> getContext(), "entry", hailStone);
+    BasicBlock * prequel = BasicBlock::Create(I -> getContext(), "prql", hailStone); 
+    BasicBlock * even = BasicBlock::Create(I -> getContext(), "even", hailStone);
+    BasicBlock * odd = BasicBlock::Create(I -> getContext(), "odd", hailStone);
+    BasicBlock * ret = BasicBlock::Create(I -> getContext(), "ret", hailStone);
+    BasicBlock * loop = BasicBlock::Create(I -> getContext(), "loop", hailStone);
+    
+    Value * one = intToVal(1, mod -> getContext());
+    Value * two = intToVal(2, mod -> getContext());
+    Value * three = intToVal(3, mod -> getContext()); 
+    
+    IRBuilder <> * builder = new IRBuilder<>(entry);
+    Value * space = builder -> CreateAlloca(
+            Type::getInt32Ty(mod -> getContext()));
+    builder -> CreateStore(n_arg, space);
+    builder -> CreateBr(prequel);
+    
+    builder -> SetInsertPoint(prequel);
+    Value * brCond = builder -> CreateICmpEQ(builder -> CreateLoad(space), one);
+    builder -> CreateCondBr(brCond,ret, loop);
+ 
+    builder -> SetInsertPoint(loop);
+    Value * checker = builder -> CreateLoad(space);
+    Value * res = builder -> CreateAnd(checker, one);
+    Value * cond = builder -> CreateICmpEQ(res, one);
+    builder -> CreateCondBr(cond, odd, even);
+        
+    builder -> SetInsertPoint(odd);
+    Value * oddChecker = builder -> CreateLoad(space);
+    Value * store = builder -> CreateAdd(
+            builder -> CreateMul(oddChecker, three), one);
+    builder -> CreateStore(store, space);
+    builder -> CreateBr(prequel);
+    
+    builder -> SetInsertPoint(even);
+    Value * evenChecker = builder -> CreateLoad(space);
+    Value * evenStore = builder -> CreateUDiv(evenChecker, two);
+    builder -> CreateStore(evenStore, space);
+    builder -> CreateBr(prequel); 
+    
+    builder -> SetInsertPoint(ret);
+    Value * retVal = builder -> CreateLoad(space); 
+    builder -> CreateRet(retVal);
+ 
+    return hailStone; 
+}
+
+Value * Insanity::insertHailStoneQuery(int n, Instruction * I){
+    Function * hailStone = generateHailStone(I);
+    IRBuilder<> * builder =  new IRBuilder<>(I);
+    return builder -> CreateCall(hailStone, intToVal(n, I));
 }
 
 void Insanity::clean_up(std::vector<Instruction*>& clean){
@@ -58,15 +130,14 @@ Value * Insanity::obfuscateXor(Instruction * I){
     IRBuilder <> * builder = new IRBuilder<>(I);
     Value * op1 = I -> getOperand(0);
     Value * op2 = I -> getOperand(1); 
-    Value * negOp1=builder 
+    Value * negOp1 = builder 
         -> CreateXor(intToVal(-1, I), op1);
-    Value* andRes1=builder -> CreateAnd(negOp1, op2);
-    Value * negOp2=builder
+    Value* andRes1 = builder -> CreateAnd(negOp1, op2);
+    Value * negOp2 = builder
         -> CreateXor(intToVal(-1, I), op2);
-    Value * andRes2=builder -> CreateAnd(op1 ,negOp2);
+    Value * andRes2 = builder -> CreateAnd(op1 ,negOp2);
     return builder -> CreateOr(andRes1, andRes2);
 }
-
 Value * Insanity::obfuscateOr(Instruction * I){
     IRBuilder <> * builder = new IRBuilder<>(I);
     Value * op1 = I -> getOperand(0);
@@ -75,8 +146,6 @@ Value * Insanity::obfuscateOr(Instruction * I){
     Value * xorRes = builder -> CreateXor(op1, op2);
     return builder -> CreateOr(andRes, xorRes);
 }
-
-
 Value * Insanity::obfuscateAnd(Instruction * I){
     IRBuilder <> * builder = new IRBuilder<>(I);
     Value * op1 = I -> getOperand(0);
@@ -86,7 +155,6 @@ Value * Insanity::obfuscateAnd(Instruction * I){
     Value * xorRes = builder -> CreateXor(op1, negOp1);
     return builder -> CreateAnd(xorRes, op1);
 }
-
 Value * Insanity::obfuscateSub(Instruction * I){
     IRBuilder <> * builder = new IRBuilder<>(I);
     Value * op1 = I -> getOperand(0);
@@ -94,11 +162,62 @@ Value * Insanity::obfuscateSub(Instruction * I){
     op2 = builder -> CreateSub(intToVal(0,I), op2); 
     return builder -> CreateAdd(op1, op2); 
 }
+Value * Insanity::obfuscateBr(BranchInst * I, Value * extra){
+    if (I -> isConditional()){
+        errs() << "Help\n";
+        Value * cond = I -> getOperand(0);
+        auto one_32 = ConstantInt::get(Type::getInt32Ty(I -> getContext()),1);
+        auto two_32= ConstantInt::get(Type::getInt32Ty(I -> getContext()),2);
+        auto three_32= ConstantInt::get(Type::getInt32Ty(I -> getContext()),3);
+    
+        auto one_1 = ConstantInt::get(Type::getInt1Ty(I -> getContext()),1);
+        auto zero_1= ConstantInt::get(Type::getInt1Ty(I -> getContext()),0);
+ 
+        BasicBlock * op2 = dyn_cast<BasicBlock>(I -> getOperand(1));
+        BasicBlock * op3 = dyn_cast<BasicBlock>(I -> getOperand(2)); 
+        BasicBlock * trap = BasicBlock::Create(I -> getContext(), 
+                "trap", I -> getFunction());
+        
+        BasicBlock * trap2 = BasicBlock::Create(I -> getContext(), 
+                "trap2", I -> getFunction());
+ 
+        IRBuilder<> * trapBuilder = new IRBuilder<>(trap);
+        ConstantInt * special = cast<ConstantInt>(extra);
+        auto sw = trapBuilder -> CreateSwitch(special, op2, 2);
+        sw -> addCase(one_32, trap);
+        sw -> addCase(two_32, op3);
+        
+        trapBuilder -> SetInsertPoint(trap2);
+        Value * tr = trapBuilder -> CreateAnd(extra, intToVal(rand() % 150,I));  
+        Value * ch = trapBuilder -> CreateXor(three_32, tr);
+        ConstantInt * swCase = cast<ConstantInt>(trapBuilder -> CreateAShr(ch, 3));
+        Value * spCond = trapBuilder -> CreateICmpUGT(
+                swCase, intToVal(rand() % 326,I ));
+
+        auto tr2 = trapBuilder -> CreateSwitch(spCond, op2);
+                tr2 -> addCase(one_1, trap);
+                tr2 -> addCase(zero_1, op3);
+
+        IRBuilder <> * builder = new IRBuilder<>(I);
+
+        auto longCon = 
+            cast<ConstantInt>
+            (builder -> CreateZExt(cond, Type::getInt32Ty(I -> getContext())));
+        
+        auto switcher = builder -> CreateSwitch(longCon, op2,3);
+        switcher -> addCase(one_32, op3);
+        switcher -> addCase(three_32, trap);
+        switcher -> addCase(two_32, trap2);
+        return switcher;
+    }
+    return extra;
+}
 
 bool Insanity::runOnFunction(Function &F){
     auto I = inst_begin(F), E = inst_end(F);
     bool modified = false;
     std::vector<Instruction *> trash;
+    if (I -> getFunction() != generateHailStone(&*I)){
     for(; I != E; ++I){
         switch(I -> getOpcode()){
             case(Instruction::Add): {
@@ -136,10 +255,24 @@ bool Insanity::runOnFunction(Function &F){
                 modified = true;
                 break;
             }
+            case(Instruction::Br): {
+                auto& ins = *I; 
+                BranchInst * br = cast<BranchInst>(&ins); 
+                if (br -> isConditional()){    
+                    srand(time(0));
+                    int n = rand();
+                    Value * retVal = insertHailStoneQuery(n, &ins);
+                    obfuscateBr(br, retVal);
+                    trash.push_back(br);                    
+                    modified = true;
+                }
+                break;
+            }
         }
+        }    
+        dumpAll(F);
         }
     clean_up(trash);
-    dumpAll(F);
     return modified;
 }
 
@@ -149,6 +282,5 @@ void Insanity::dumpAll(Function &F) const{
             I.dump();
 }
 }
-
 char Insanity::ID = 0;
-static RegisterPass<Insanity> X("1nsanity","Insanity");
+static RegisterPass<Insanity> X("1nsanity","Insanity"); 
--- test.c
+++ test.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(){
+int main(){ 
+    /*
+    int a = 0;
+    if (a == 0){
+        a = 5;
+    }
+    printf("%d\n",a);
+    */
+    /*
     int a, b, c, d, e;
     a = 17;
     b = 3;
@@ -16,5 +24,40 @@ int main(){
     printf("%d \n",c);
     d = c - 25;
     printf("%d \n",d);
-    return 0;
+	*/
+    /*
+    int a = 1;
+	int b = 0;
+	  while(1) {
+		switch(b) {
+		  case 0:
+			if(a == 0)
+			  b = 1;
+			else
+			  b = 2;
+			break;
+		  case 1:
+            printf("%d %d\n", a,b);
+			return 1;
+		  case 2:
+            printf("%d %d\n", a,b);
+			return 10;
+		  default:
+			break;
+		}
+	  }
+	  return 0;
+    */
+    int a = 5;
+    while (a != 1){
+        printf("%d\n", a);
+        if ((a & 1) == 1){
+            a *= 3;
+            a += 1;
+        }
+        else{
+            a /= 2;
+        }
+    }
+    return a;
 }
